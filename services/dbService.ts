a1c7eac52df4786b8dbc32a3738396c0b8b4fa17
Ravinder Singh <ravinder.singh@groww.in>
2024-09-29 22:02:45 +0530

    chore: add insert & retrieve data methods in db service

--- services/dbService.ts
+++ services/dbService.ts
@@ -1,38 +1,80 @@
 import * as SQLite from "expo-sqlite";
 import { ArticleType } from "../types/news";
 
-class DBServices {
+class DBService {
     #isDbInitialized = false;
+    #isTableCreated = false;
     #db: SQLite.SQLiteDatabase | null = null;
 
-    constructor() {
-        (async () => {
-            await this.dbInit();
-            await this.createTable();
-        })();
-    }
+    NAME = "DB Service";
+
+    constructor() {}
 
     async dbInit() {
         if (this.#isDbInitialized === false) {
-            this.#db = await SQLite.openDatabaseAsync("databaseName");
-            this.#isDbInitialized = true;
+            try {
+                this.#db = await SQLite.openDatabaseAsync("databaseName");
+                this.#isDbInitialized = true;
+                console.log(`${this.NAME} db connection opened`);
+            } catch (err) {
+                console.log("Error", err);
+            }
         }
     }
 
     async createTable() {
-        await this.#db?.execAsync(`
-            CREATE TABLE IF NOT EXISTS news (
-                id INTEGER PRIMARY KEY NOT NULL,
-                author TEXT NOT NULL,
-                title TEXT,
-                description TEXT,
-                url TEXT,
-                urlToImage TEXT,
-                publishedAt TEXT,
-                content TEXT
-            );
-            `);
+        if (this.#isTableCreated == false) {
+            try {
+                await this.#db?.execAsync(`
+                    CREATE TABLE IF NOT EXISTS news (
+                        id INTEGER PRIMARY KEY NOT NULL,
+                        author TEXT NOT NULL,
+                        title TEXT,
+                        source TEXT,
+                        description TEXT,
+                        url TEXT,
+                        urlToImage TEXT,
+                        publishedAt TEXT,
+                        content TEXT
+                    );
+                `);
+                console.log(`${this.NAME} table create success`);
+                this.#isTableCreated = true;
+            } catch (err) {
+                console.log("Error", err);
+            }
+        }
     }
 
-    async insertAllNews(data: Array<ArticleType>) {}
+    async insertAllNews() {
+        try {
+            await this.dbInit();
+            await this.createTable();
+
+            await this.#db?.execAsync(`
+                INSERT INTO news (author ,title ,source ,description ,url ,urlToImage ,publishedAt ,content)
+                VALUES ("ravinder singh", "Hero", "Mint", "Hero h ji hero", "not present", "not present", "At world start", "super vala super hero h apna bhai");
+            `);
+            console.log(`${this.NAME} data insert success`);
+        } catch (err) {
+            console.error("Error in inserting data", err);
+        }
+    }
+
+    async getAllNews() {
+        try {
+            await this.dbInit();
+            await this.createTable();
+
+            const data = await this.#db?.getAllAsync(`
+                SELECT * from news;
+            `);
+            return data;
+        } catch (err) {
+            console.error("Error in getting data", err);
+        }
+    }
 }
+
+const dbServiceInstance = new DBService();
+export { dbServiceInstance };
