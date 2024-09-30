1714bf8f76fda4ed4b578639b58c10deebc9135f
Ravinder Singh <ravindermaan0123@gmail.com>
2024-10-01 01:49:26 +0530

    chore: add loading state & implement refresh timer logic

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemedText } from "../components/ThemedText";
 import { ThemedView } from "../components/ThemedView";
 import {
+    ActivityIndicator,
     Alert,
     Dimensions,
     FlatList,
@@ -11,38 +12,37 @@ import {
     View,
 } from "react-native";
 import { EvilIcons } from "@expo/vector-icons";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ArticleType } from "../types/news";
 import dbService, { DBService, dbServiceInstance } from "../services/dbService";
+import {
+    GestureHandlerRootView,
+    RectButton,
+    Swipeable,
+} from "react-native-gesture-handler";
+import Animated, {
+    Extrapolation,
+    interpolate,
+    SharedValue,
+    useAnimatedStyle,
+} from "react-native-reanimated";
+import NewsListItem from "../components/news/newsListItem";
 
 const data = [{}];
 // curl --location --request POST 'https://asia-south1-kc-stage-rp.cloudfunctions.net/globalNews?endpoint=everything&q=tesla&from=2024-07-27&sortBy=publishedAt' \
 // --header 'Accept: application/json, text/plain, */*' \
 // --data ''
 
-const API_URL = `https://newsapi.org/v2/everything?q=india&from=2024-08-29&sortBy=publishedAt&page=1&pageSize=100&apiKey=${process.env.EXPO_PUBLIC_NEWS_API_KEY}`;
-
-function getFormattedDate(dateString: string) {
-    const date = new Date(dateString);
-
-    // Extract hours, minutes, and seconds
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    const ampm = hours >= 12 ? "PM" : "AM";
-
-    // Format the time (adding leading zeros if necessary)
-    const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")} ${ampm}`;
-
-    return formattedTime;
-}
+const todayMonth = new Date().getMonth().toString().padStart(2, "0");
+const API_URL = `https://newsapi.org/v2/everything?q=india&from=2024-${todayMonth}-01&sortBy=publishedAt&page=1&pageSize=100&apiKey=${process.env.EXPO_PUBLIC_NEWS_API_KEY}`;
 
 export default function NewsScreen() {
     const sWidth = Dimensions.get("window").width;
     const [listData, setListData] = useState<Array<ArticleType>>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const offset = useRef(0);
+    const PAGE_SIZE = 5;
 
     const fetchFromNetowrk = async (): Promise<Array<ArticleType>> => {
         try {
@@ -67,13 +67,43 @@ export default function NewsScreen() {
             console.log("Serving data from local db");
         }
 
-        const displayData = await dbServiceInstance.getAllNews();
+        const displayData = await dbServiceInstance.getBatchedNews(
+            offset.current,
+            PAGE_SIZE
+        );
+        offset.current += PAGE_SIZE;
+
         setListData(displayData);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+    };
+
+    const startRefreshTimers = () => {
+        const id = setInterval(() => {
+            console.log("Refresh request received");
+
+            dbServiceInstance
+                .getBatchedNews(offset.current, PAGE_SIZE)
+                .then((data) => {
+                    console.log(
+                        "data refreshed",
+                        data.map((dd) => dd?.id)
+                    );
+                    offset.current += PAGE_SIZE;
+                    setListData(data);
+                });
+        }, 5000); // change to 10 sec for now 5 secs
+
+        return id;
     };
 
     useEffect(() => {
         console.log("API", API_URL);
         fetchData();
+        const timerId = startRefreshTimers();
+
+        return () => clearInterval(timerId);
     }, []);
 
     const onRefreshPress = () => {
@@ -88,56 +118,47 @@ export default function NewsScreen() {
     };
     // console.log("re-render API data", apiData);
 
+    if (isLoading) {
+        return (
+            <ThemedView
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <ActivityIndicator size={"large"} />
+            </ThemedView>
+        );
+    }
+
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-            <ThemedView style={styles.header}>
-                <Image
-                    source={require("../assets/images/company_logo.png")}
-                    resizeMode="cover"
-                    style={styles.header_image}
-                />
-                <TouchableOpacity onPress={onRefreshPress}>
-                    <EvilIcons name="refresh" size={40} />
-                </TouchableOpacity>
-            </ThemedView>
-            <ThemedView>
-                <FlatList
-                    data={listData}
-                    keyExtractor={(item) => item.url}
-                    renderItem={({ item }) => {
-                        return (
-                            <View style={styles.news_item}>
-                                <View style={styles.news_item_left}>
-                                    <ThemedText style={styles.subtle_text}>
-                                        {item.source.name}
-                                    </ThemedText>
-                                    <ThemedText
-                                        style={styles.news_item_title}
-                                        numberOfLines={3}
-                                        ellipsizeMode="tail"
-                                    >
-                                        {item.title}
-                                    </ThemedText>
-                                    <ThemedText style={styles.subtle_text}>
-                                        {item.author}
-                                    </ThemedText>
-                                </View>
-                                <View style={styles.news_item_right}>
-                                    <ThemedText style={styles.subtle_text_dark}>
-                                        {getFormattedDate(item.publishedAt)}
-                                    </ThemedText>
-                                    <Image
-                                        source={{ uri: item.urlToImage }}
-                                        style={styles.news_item_image}
-                                    />
-                                </View>
-                            </View>
-                        );
-                    }}
-                    ItemSeparatorComponent={<View style={styles.seperator} />}
-                />
-            </ThemedView>
-        </SafeAreaView>
+        <GestureHandlerRootView>
+            <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+                <ThemedView style={styles.header}>
+                    <Image
+                        source={require("../assets/images/company_logo.png")}
+                        resizeMode="cover"
+                        style={styles.header_image}
+                    />
+                    <TouchableOpacity onPress={onRefreshPress}>
+                        <EvilIcons name="refresh" size={40} />
+                    </TouchableOpacity>
+                </ThemedView>
+                <ThemedView>
+                    <FlatList
+                        data={listData}
+                        keyExtractor={(item) => item.url}
+                        renderItem={({ item }) => {
+                            return <NewsListItem item={item} />;
+                        }}
+                        ItemSeparatorComponent={
+                            <View style={styles.seperator} />
+                        }
+                    />
+                </ThemedView>
+            </SafeAreaView>
+        </GestureHandlerRootView>
     );
 }
 
